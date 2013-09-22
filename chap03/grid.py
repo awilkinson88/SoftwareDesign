c86df769eb0a8546f12dac4b25ad080f2b09291f
Anne <awilkinson@awilkinson-Latitude-E6420.(none)>
2013-09-22 14:24:11 -0400

    Homework 3 Grid

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,24 @@
 """Solution to an exercise in Think Python.
 
-Author:
+Author: Anne Wilkinson
 """
 
-print '+ - - - -'
+def draw_horiz ():
+	print ('+ - - - - + - - - - +')
+
+def draw_vert ():
+	print ('|         |         |')
+
+def draw_twobyone ():
+    draw_horiz ()
+    draw_vert ()
+    draw_vert ()
+    draw_vert ()
+    draw_vert ()
+
+def draw_grid ():
+    draw_twobyone()
+    draw_twobyone()
+    draw_horiz ()
+
+draw_grid ()
